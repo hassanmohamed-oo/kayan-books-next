9916b31bcbb8c8e01f29f4218889a64aeae31ae7
hassan mohamed <hassancsstu@gmail.com>
2025-02-02 05:09:21 +0200

    Refactor component exports for improved readability and consistency

--- components/index.tsx
+++ components/index.tsx
@@ -1,9 +1,17 @@
 import { Navbar } from "./Navbar";
 import { Footer } from "./Footer";
 import { Landing } from "./Landing";
-import { Bookspage } from "@/app/Books/page";
+import { Bookspage } from "@/app/books/page";
 import { Techoffers } from "@/app/techoffers/page";
 import { Examscontrol } from "./Examscontrol";
 import { BooksControl } from "./bookscontrol";
 
-export { Navbar, Footer, Landing, Bookspage, Techoffers, Examscontrol, BooksControl };
+export {
+  Navbar,
+  Footer,
+  Landing,
+  Bookspage,
+  Techoffers,
+  Examscontrol,
+  BooksControl,
+};
