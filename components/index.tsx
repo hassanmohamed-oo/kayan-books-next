8a2565f44851e538ffa3576fd8c009dd6620219d
hassan mohamed <hassancsstu@gmail.com>
2025-02-02 14:32:34 +0200

    up

--- components/index.tsx
+++ components/index.tsx
@@ -2,7 +2,7 @@ import { Navbar } from "./Navbar";
 import { Footer } from "./Footer";
 import { Landing } from "./Landing";
 
-import { Techoffers } from "@/app/techoffers/page";
+import { Techoffers } from "@/app/Techoffers/page";
 import { Examscontrol } from "./Examscontrol";
 import { BooksControl } from "./bookscontrol";
 
